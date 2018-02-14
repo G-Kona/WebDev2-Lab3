5c27e246ee240475a39a1a662b47931fae30e235
Geri Kona <gerikona@hotmail.com>
2018-02-14 10:09:10 -0500

    Upload Lab

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer id="footer">
+    <div class="wrap">
+        <p>Copyright &copy; 2018 Pancake City</p>
+    </div>
+</footer>
+<!--Used to animate star rating selection-->
+<script src="script.js"></script>
+</body>
+</html>
